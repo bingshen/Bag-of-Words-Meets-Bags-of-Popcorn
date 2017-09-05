51fe662bc3ce101f7c6eb76e46b8bad71a44df1a
bingshen <510685263@qq.com>
2017-09-05 18:33:50 +0800

    第二模型提交

--- feature_d2v.py
+++ feature_d2v.py
@@ -6,18 +6,22 @@ from gensim.models import Doc2Vec
 from gensim.models.doc2vec import LabeledSentence
 import os
 import nltk
+from numpy import *
 
-def getCleanLabeledReviews(reviews):
-    clean_reviews = []
-    for review in reviews["review"]:
-        clean_reviews.append(Word2VecUtil.review_to_wordlist(review))
-    labelized = []
-    for i, id_label in enumerate(reviews["id"]):
-        labelized.append(LabeledSentence(clean_reviews[i], [id_label]))
-        print(labelized)
-        os.system('pause')
-    return labelized
+def get_label_sentence(dataframe):
+    label_sentence=[]
+    for [review,label_id] in dataframe[['review','id']].values:
+        wordlist=Word2Vec.review_to_wordlist(review)
+        label_sentence.append(LabeledSentence(words=wordlist,tags=[label_id]))
+    return label_sentence
 
 if __name__ == '__main__':
-    train  = pd.read_csv('data\\labeledTrainData.tsv', header=0, delimiter="\t", quoting=3)
-    train_reviews = getCleanLabeledReviews(train)
+    labeled_df=pd.read_csv("data\\labeledTrainData.tsv",delimiter="\t",quoting=3)
+    unlabeled_df=pd.read_csv("data\\unlabeledTrainData.tsv",delimiter="\t",quoting=3)
+    test_df=pd.read_csv("data\\testData.tsv",delimiter="\t",quoting=3)
+    model_dm="1000features_1minwords_10context_dm"
+    model_bow="1000features_1minwords_10context_bow"
+    labeled_sentence=get_label_sentence(labeled_df)
+    unlabeled_sentence=get_label_sentence(unlabeled_df)
+    test_sentence=get_label_sentence(test_df)
+    total_train=vstack((labeled_sentence,unlabeled_sentence,test_sentence))
