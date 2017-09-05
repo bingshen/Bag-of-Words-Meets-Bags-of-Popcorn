65d85b05c26173f508363b247de09d66da451375
bingshen <510685263@qq.com>
2017-09-05 15:25:43 +0800

    第二个模型

--- feature_d2v.py
+++ feature_d2v.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from Word2VecUtil import Word2VecUtil
 from gensim.models import Word2Vec
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import LabeledSentence
 import os
 import nltk
 
 def getCleanLabeledReviews(reviews):
     clean_reviews = []
     for review in reviews["review"]:
-        clean_reviews.append(KaggleWord2VecUtility.review_to_wordlist(review))
+        clean_reviews.append(Word2VecUtil.review_to_wordlist(review))
     labelized = []
     for i, id_label in enumerate(reviews["id"]):
         labelized.append(LabeledSentence(clean_reviews[i], [id_label]))
@@ -17,5 +19,5 @@ def getCleanLabeledReviews(reviews):
     return labelized
 
 if __name__ == '__main__':
-    train  = pd.read_csv('../data/labeledTrainData.tsv', header=0, delimiter="\t", quoting=3)
+    train  = pd.read_csv('data\\labeledTrainData.tsv', header=0, delimiter="\t", quoting=3)
     train_reviews = getCleanLabeledReviews(train)
