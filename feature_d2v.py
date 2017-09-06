720ad5fbc700db414ebd1a878b80a517e618f071
bingshen <510685263@qq.com>
2017-09-06 14:58:28 +0800

    d2v模型+预测

--- feature_d2v.py
+++ feature_d2v.py
@@ -11,7 +11,7 @@ from numpy import *
 def get_label_sentence(dataframe):
     label_sentence=[]
     for [review,label_id] in dataframe[['review','id']].values:
-        wordlist=Word2Vec.review_to_wordlist(review)
+        wordlist=Word2VecUtil.review_to_wordlist(review)
         label_sentence.append(LabeledSentence(words=wordlist,tags=[label_id]))
     return label_sentence
 
@@ -19,9 +19,19 @@ if __name__ == '__main__':
     labeled_df=pd.read_csv("data\\labeledTrainData.tsv",delimiter="\t",quoting=3)
     unlabeled_df=pd.read_csv("data\\unlabeledTrainData.tsv",delimiter="\t",quoting=3)
     test_df=pd.read_csv("data\\testData.tsv",delimiter="\t",quoting=3)
-    model_dm="1000features_1minwords_10context_dm"
-    model_bow="1000features_1minwords_10context_bow"
+    model_dm_name="1000features_1minwords_10context_dm"
+    model_bow_name="1000features_1minwords_10context_bow"
     labeled_sentence=get_label_sentence(labeled_df)
     unlabeled_sentence=get_label_sentence(unlabeled_df)
     test_sentence=get_label_sentence(test_df)
     total_train=vstack((labeled_sentence,unlabeled_sentence,test_sentence))
+    model_dm=Doc2Vec(workers=8,size=1000,min_count=1,window=10)
+    model_bow=Doc2Vec(workers=8,size=1000,min_count=1,window=10,dm=0)
+    model_dm.build_vocab(total_train)
+    model_bow.build_vocab(total_train)
+    for epoch in range(10):
+        perm=random.permutation(total_train.shape[0])
+        model_dm.train(total_train[perm])
+        model_bow.train(total_train[perm])
+    model_dm.save(model_dm_name)
+    model_bow.save(model_bow_name)
---
+++ predict_d2v.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+from Word2VecUtil import Word2VecUtil
+from gensim.models import Word2Vec
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import LabeledSentence
+from sklearn.linear_model import LogisticRegression
+import os
+import nltk
+from numpy import *
+
+def get_data_array(model,dataframe):
+    data_array=zero((dataframe.values.shape[0],1000))
+    for (i,label_id) in enumerate(dataframe['review'].values):
+        data_array[i,:]=model[label_id]
+    return data_array
+
+if __name__ == '__main__':
+    labeled_df=pd.read_csv("data\\labeledTrainData.tsv",delimiter="\t",quoting=3)
+    test_df=pd.read_csv("data\\testData.tsv",delimiter="\t",quoting=3)
+    model_dm_name="1000features_1minwords_10context_dm"
+    model_bow_name="1000features_1minwords_10context_bow"
+    model_dm=Doc2Vec.load(model_dm_name)
+    model_bow=Doc2Vec.load(model_bow_name)
+    dm_train_x=get_data_array(model_dm,labeled_df)
+    bow_train_x=get_data_array(model_bow,labeled_df)
+    dm_test_x=get_data_array(model_dm,test_df)
+    bow_test_x=get_data_array(model_bow,test_df)
+    train_x=hstack((dm_train_x,bow_train_x))
+    train_y=labeled_df['sentiment'].values
+    test_x=hstack((dm_test_x,bow_test_x))
+    lr_model=LogisticRegression()
+    lr_model.fit(train_x,train_y)
+    pred_y=lr_model.predict(test_x)
+    submission=pd.DataFrame({'id':test_df['id'],'sentiment':pred_y})
+    submission.to_csv('submission.csv',index=False,quoting=3)
